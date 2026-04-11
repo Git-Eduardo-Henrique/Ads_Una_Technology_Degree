995cb5f0ba34cdf74b56381ab0d967f5269d5cce
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2026-04-11 17:40:30 -0300

    conjuntos_ex_03

---
+++ MATE_COMP_APLICADA/conjuntos/ex03.py
@@ -0,0 +1,18 @@
+u = {1,2,3,4,5,6,7,8}
+a = {1,3,5}
+b = {2,5,8}
+
+print(50*"-=")
+print(f"U = {u}\nA = {a}\nB = {b}")
+print(50*"-=")
+
+# | = União
+print(f"a) A ∪ B = {a|b}")
+
+# & = Interseção
+print(f"b) A ∩ B = {a&b}")
+
+# - = Diferença
+print(f"c) A - B = {a-b}")
+print(f"d) B - A = {b-a}")
+print(50*"-=")
